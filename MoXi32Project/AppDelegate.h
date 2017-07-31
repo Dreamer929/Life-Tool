eed44203132db0331e80c7fe7ac227effb136720
moxi <moxi@moxideMacBook-Pro.local>
2017-07-31 15:15:30 +0800

    Initial Commit

---
+++ MoXi32Project/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MoXi32Project
+//
+//  Created by moxi on 2017/7/31.
+//  Copyright © 2017年 zyf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MoXi32Project/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MoXi32Project
+//
+//  Created by moxi on 2017/7/31.
+//  Copyright © 2017年 zyf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
